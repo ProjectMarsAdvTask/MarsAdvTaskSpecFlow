750987a32e554f38e544ce69c7c965967623fc1e
JL_Yeoh <37150682+WOD56Tech0@users.noreply.github.com>
2022-05-11 22:34:54 +0800

    Added of the profile desciription pages

---
+++ MarsAdvTaskSpecFlow/MarsAdvTaskSpecFlow/Pages/ProfilePage/ProfileDescription.cs
@@ -0,0 +1,63 @@
+﻿using MarsAdvTaskSpecFlow.Utils;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using System;
+
+namespace MarsAdvTaskSpecFlow.Pages.ProfilePage
+{
+    class ProfileDescription
+    {
+        private IWebDriver testDriver;
+        private IWebElement descriptionIcon => testDriver.FindElement(By.XPath("//*[@id='account-profile-section']/div/section[2]/div/div/div/div[3]/div/div/div/h3/span/i"));
+        private IWebElement descriptionTextBox => testDriver.FindElement(By.XPath("//*[@id='account-profile-section']/div/section[2]/div/div/div/div[3]/div/div/form/div/div/div[2]/div[1]/textarea"));
+        private IWebElement saveButton => testDriver.FindElement(By.XPath("//*[@id='account-profile-section']/div/section[2]/div/div/div/div[3]/div/div/form/div/div/div[2]/button"));
+        private IWebElement notification => testDriver.FindElement(By.XPath("//div[@class='ns-box-inner']"));
+
+        public void DescriptionPenIcon(IWebDriver testDriver)
+        {
+            this.testDriver = testDriver;
+            // Identify Description icon and click
+            WaitHelpers.WaitForElementToBeClickable(testDriver, "XPath", "//*[@id='account-profile-section']/div/section[2]/div/div/div/div[3]/div/div/div/h3/span/i", 4);
+            descriptionIcon.Click();
+            testDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
+        }
+
+        public void AddDescription(IWebDriver testDriver, string Description)
+        {
+            this.testDriver = testDriver;
+            Actions Hover = new Actions(testDriver);
+
+            // Identify the description textarea enter valid description
+            WaitHelpers.WaitForElementPresent(testDriver, "XPath", "//*[@id='account-profile-section']/div/section[2]/div/div/div/div[3]/div/div/form/div/div/div[2]/div[1]/textarea", 2);
+            Hover.Click(descriptionTextBox).Perform();
+            descriptionTextBox.Clear();
+            descriptionTextBox.SendKeys(Description);
+
+            // Click on "Save" button
+            WaitHelpers.WaitForElementToBeClickable(testDriver, "XPath", "//*[@id='account-profile-section']/div/section[2]/div/div/div/div[3]/div/div/form/div/div/div[2]/button", 5);
+            saveButton.Click();
+        }
+
+        public string GetDescriptnotification(IWebDriver testDriver)
+        {
+            WaitHelpers.WaitForElementToBeClickable(testDriver, "XPath", "//div[@class='ns-box-inner']", 3);
+            return notification.Text;
+        }
+
+        public void EditDescription(IWebDriver testDriver, string Description)
+        {
+            this.testDriver = testDriver;
+            Actions Hover = new Actions(testDriver);
+
+            // Identify the description textarea enter valid description
+            WaitHelpers.WaitForElementPresent(testDriver, "XPath", "//*[@id='account-profile-section']/div/section[2]/div/div/div/div[3]/div/div/form/div/div/div[2]/div[1]/textarea", 2);
+            Hover.Click(descriptionTextBox).Perform();
+            descriptionTextBox.Clear();
+            descriptionTextBox.SendKeys(Description);
+
+            // Click on "Save" button
+            WaitHelpers.WaitForElementToBeClickable(testDriver, "XPath", "//*[@id='account-profile-section']/div/section[2]/div/div/div/div[3]/div/div/form/div/div/div[2]/button", 5);
+            saveButton.Click();
+        }
+    }
+}
---
+++ MarsAdvTaskSpecFlow/MarsAdvTaskSpecFlow/SpecFlow/StepDefinition/ProfilePage/ProfileDescriptionSteps.cs
@@ -0,0 +1,62 @@
+﻿using MarsAdvTaskSpecFlow.Pages.ProfilePage;
+using MarsAdvTaskSpecFlow.Utils;
+using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using TechTalk.SpecFlow;
+
+namespace MarsAdvTaskSpecFlow.SpecFlow.StepDefinition.ProfilePage
+{
+    [Binding]
+    public class ProfileDescriptionSteps : CommonDriver
+    {
+        // Login Page object initialization and definition
+        LoginPage loginPageObj = new LoginPage();
+
+        // Login Page object initialization and definition
+        ProfileDescription profileDetailsObj = new ProfileDescription();
+
+        [Given(@"I login to Trade Skills portal description successfully")]
+        public void GivenILoginToTradeSkillsPortalDescriptionSuccessfully()
+        {
+            // Open chrome browser
+            testDriver = new ChromeDriver();
+            loginPageObj.LoginPageSteps(testDriver);
+        }
+
+        [Given(@"I click on the description pen icon")]
+        public void GivenIClickOnTheDescriptionPenIcon()
+        {
+            profileDetailsObj.DescriptionPenIcon(testDriver);
+        }
+
+        [When(@"I add the '(.*)' and click on Save button")]
+        public void WhenIAddTheAndClickOnSaveButton(string Description)
+        {
+            profileDetailsObj.AddDescription(testDriver, Description);
+        }
+
+        [Then(@"the Description should be added as successfully")]
+        public void ThenTheDescriptionShouldBeAddedAsSuccessfully()
+        {
+            string notification = profileDetailsObj.GetDescriptnotification(testDriver);
+
+            // Assertion that Description notification has been displayed.
+            Assert.AreEqual(notification, "Description has been saved successfully");
+        }
+
+        [When(@"I edit the '(.*)' and click on Save button")]
+        public void WhenIEditTheAndClickOnSaveButton(string Description)
+        {
+            profileDetailsObj.EditDescription(testDriver, Description);
+        }
+
+        [Then(@"the Description should be updated as successfully")]
+        public void ThenTheDescriptionShouldBeUpdatedAsSuccessfully()
+        {
+            string notification = profileDetailsObj.GetDescriptnotification(testDriver);
+
+            // Assertion that Description notification has been displayed.
+            Assert.AreEqual(notification, "Description has been saved successfully");
+        }
+    }
+}
