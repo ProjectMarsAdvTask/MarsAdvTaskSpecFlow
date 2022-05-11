eb5e09d99361016da00f1a6626769bd21f0b5ad7
JL_Yeoh <37150682+WOD56Tech0@users.noreply.github.com>
2022-05-11 22:32:20 +0800

    Added of the profile view page

---
+++ MarsAdvTaskSpecFlow/MarsAdvTaskSpecFlow/SpecFlow/StepDefinition/ProfilePage/ProfileViewPageSteps.cs
@@ -0,0 +1,52 @@
+﻿using MarsAdvTaskSpecFlow.Pages;
+using MarsAdvTaskSpecFlow.Pages.ProfilePage;
+using MarsAdvTaskSpecFlow.Utils;
+using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using TechTalk.SpecFlow;
+
+namespace MarsAdvTaskSpecFlow.SpecFlow.StepDefinition.ProfilePage
+{
+    [Binding]
+    public class ProfileViewPageSteps : CommonDriver
+    {
+        // Profile View object initialization and definition
+        ProfileViewPage ProfileViewPageObj = new ProfileViewPage();
+
+        // Login Page object initialization and definition
+        LoginPage loginPageObj = new LoginPage();
+
+        // Home Page object initialization and definition
+        HomePage NaviHomePageObj = new HomePage();
+
+        [Given(@"I login to the Trade Skills portal of view successfully")]
+        public void GivenILoginToTheTradeSkillsPortalOfViewSuccessfully()
+        {
+            // Open chrome browser
+            testDriver = new ChromeDriver();
+            loginPageObj.LoginPageSteps(testDriver);
+        }
+        
+        [Given(@"I navigate to the main page and click on Programming & Tech")]
+        public void GivenINavigateToTheMainPageAndClickOnProgrammingTech()
+        {
+            ProfileViewPageObj.GoToMainPage(testDriver);
+            ProfileViewPageObj.NavigateToImgIcon(testDriver);
+        }
+        
+        [When(@"I navigate to the seller profile page")]
+        public void WhenINavigateToTheSellerProfilePage()
+        {
+            NaviHomePageObj.NaviHomePage(testDriver);
+        }
+
+        [Then(@"seller profile page should be updated successfully")]
+        public void ThenSellerProfilePageShouldBeUpdatedSuccessfully()
+        {
+            string validateUser = ProfileViewPageObj.ValidateUser(testDriver);
+
+            // Assertion that Seller record has been created.
+            Assert.AreEqual(validateUser, validateUser, "Actual user and expected user don't match");
+        }
+    }
+}
