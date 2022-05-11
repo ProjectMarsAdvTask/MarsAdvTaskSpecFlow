e2a30522f47efc1f391591773aa121434b69540f
JL_Yeoh <37150682+WOD56Tech0@users.noreply.github.com>
2022-05-11 22:33:43 +0800

    Added of the profile certification pages

---
+++ MarsAdvTaskSpecFlow/MarsAdvTaskSpecFlow/Pages/ProfilePage/ProfileCertification.cs
@@ -0,0 +1,104 @@
+﻿using MarsAdvTaskSpecFlow.Utils;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+
+namespace MarsAdvTaskSpecFlow.SpecFlow.StepDefinition.ProfilePage
+{
+    class ProfileCertification
+    {
+        private IWebDriver testDriver;
+        private IWebElement notification => testDriver.FindElement(By.XPath("//div[@class='ns-box-inner']"));
+        private IWebElement addCertification => testDriver.FindElement(By.XPath("//div[@class='ui bottom attached tab segment tooltip-target active']//thead/tr/th[4]/div"));
+        private IWebElement addCertificationBtn => testDriver.FindElement(By.XPath("//input[@value='Add']"));
+        private IWebElement updateCertification => testDriver.FindElement(By.XPath("//*[@id='account-profile-section']/div/section[2]/div/div/div/div[3]/form/div[5]/div[1]/div[2]/div/table/tbody/tr/td[4]/span[1]/i"));
+        private IWebElement updateCertificationBtn => testDriver.FindElement(By.XPath("//input[@value='Update']"));
+        private IWebElement deleteCertification => testDriver.FindElement(By.XPath("//i[@class='remove icon']"));
+        private IWebElement certificationTextBox1 => testDriver.FindElement(By.Name("certificationName"));
+        private IWebElement certificationTextBox2 => testDriver.FindElement(By.Name("certificationFrom"));
+        private SelectElement levelDDList => new SelectElement(testDriver.FindElement(By.Name("certificationYear")));
+
+        public void AddNewCertBtn(IWebDriver testDriver)
+        {
+            this.testDriver = testDriver;
+            // Click on the "Add new" button of certification
+            WaitHelpers.WaitForElementToBeClickable(testDriver, "XPath", "//div[@class='ui bottom attached tab segment tooltip-target active']//thead/tr/th[4]/div", 4);
+            addCertification.Click();
+            testDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
+        }
+
+        public string GetNotification(IWebDriver testDriver)
+        {
+            WaitHelpers.WaitForElementToBeClickable(testDriver, "XPath", "//div[@class='ns-box-inner']", 4);
+            return notification.Text;
+        }
+
+        public void AddCertification(IWebDriver testDriver, string Certificate, string From, string Year)
+        {
+            this.testDriver = testDriver;
+
+            // Identify the certification textbox enter valid certification
+            WaitHelpers.WaitForElementPresent(testDriver, "Name", "certificationName", 2);
+            certificationTextBox1.Click();
+            //certificationTextBox1.Clear();
+            certificationTextBox1.SendKeys(Certificate);
+
+            // Identify the certification textbox enter valid certification
+            WaitHelpers.WaitForElementPresent(testDriver, "Name", "certificationFrom", 2);
+            certificationTextBox2.Click();
+            //certificationTextBox2.Clear();
+            certificationTextBox2.SendKeys(From);
+
+            // Identify the year from the dropdown list
+            WaitHelpers.WaitForElementPresent(testDriver, "Name", "certificationYear", 2);
+            levelDDList.SelectByValue(Year);
+
+            // Click on "Add" button
+            WaitHelpers.WaitForElementToBeClickable(testDriver, "XPath", "//input[@value='Add']", 5);
+            addCertificationBtn.Click();
+        }
+
+        public void EditNewCertBtn(IWebDriver testDriver)
+        {
+            this.testDriver = testDriver;
+            // Click on edit pen icon of the language
+            WaitHelpers.WaitForElementToBeClickable(testDriver, "XPath", "//*[@id='account-profile-section']/div/section[2]/div/div/div/div[3]/form/div[5]/div[1]/div[2]/div/table/tbody/tr/td[4]/span[1]/i", 4);
+            updateCertification.Click();
+            testDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
+        }
+
+        public void EditCertification(IWebDriver testDriver, string Certificate, string From, string Year)
+        {
+            this.testDriver = testDriver;
+
+            // Identify the certification textbox enter valid certification
+            WaitHelpers.WaitForElementPresent(testDriver, "Name", "certificationName", 2);
+            certificationTextBox1.Click();
+            certificationTextBox1.Clear();
+            certificationTextBox1.SendKeys(Certificate);
+
+            // Identify the certification textbox enter valid certification
+            WaitHelpers.WaitForElementPresent(testDriver, "Name", "certificationFrom", 2);
+            certificationTextBox2.Click();
+            certificationTextBox2.Clear();
+            certificationTextBox2.SendKeys(From);
+
+            // Identify the year from the dropdown list
+            WaitHelpers.WaitForElementPresent(testDriver, "Name", "certificationYear", 2);
+            levelDDList.SelectByValue(Year);
+
+            // Click on "Update" button
+            WaitHelpers.WaitForElementToBeClickable(testDriver, "XPath", "//input[@value='Update']", 5);
+            updateCertificationBtn.Click();
+        }
+
+        public void DeleteNewCertBtn(IWebDriver testDriver)
+        {
+            this.testDriver = testDriver;
+            // Click on the "Delete" button of language
+            WaitHelpers.WaitForElementToBeClickable(testDriver, "XPath", "//i[@class='remove icon']", 4);
+            deleteCertification.Click();
+            testDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
+        }
+    }
+}
---
+++ MarsAdvTaskSpecFlow/MarsAdvTaskSpecFlow/SpecFlow/StepDefinition/ProfilePage/ProfileCertificationSteps.cs
@@ -0,0 +1,87 @@
+﻿using MarsAdvTaskSpecFlow.Pages;
+using MarsAdvTaskSpecFlow.Pages.ProfilePage;
+using MarsAdvTaskSpecFlow.Utils;
+using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using TechTalk.SpecFlow;
+
+namespace MarsAdvTaskSpecFlow.SpecFlow.StepDefinition.ProfilePage
+{
+    [Binding]
+    public class ProfileCertificationSteps : CommonDriver
+    {
+        // Login Page object initialization and definition
+        LoginPage loginPageObj = new LoginPage();
+
+        // Home Page object initialization and definition
+        HomePage NaviHomePageObj = new HomePage();
+
+        // Profile Certications Page object initialization and definition
+        ProfileCertification profileCertsObj = new ProfileCertification();
+
+        [Given(@"I login to the Trade Skills portal for Certs successfully")]
+        public void GivenILoginToTheTradeSkillsPortalForCertsSuccessfully()
+        {
+            // Open chrome browser
+            testDriver = new ChromeDriver();
+            loginPageObj.LoginPageSteps(testDriver);
+        }
+        
+        [Given(@"I navigate to the skiils page under Certifications tab")]
+        public void GivenINavigateToTheSkiilsPageUnderCertificationsTab()
+        {
+            NaviHomePageObj.CertificationsPage(testDriver);
+        }
+
+        [Given(@"I click on the Add New button under Certifications tab")]
+        public void GivenIClickOnTheAddNewButtonUnderCertificationsTab()
+        {
+            profileCertsObj.AddNewCertBtn(testDriver);
+        }
+
+        [When(@"I enter the details in '(.*)', '(.*)', '(.*)' and click on add button")]
+        public void WhenIEnterTheDetailsInAndClickOnAddButton(string Certificate, string From, string Year)
+        {
+            profileCertsObj.AddCertification(testDriver, Certificate, From, Year);
+        }
+
+        [Then(@"A new row should be added as in '(.*)' successfully")]
+        public void ThenANewRowShouldBeAddedAsInSuccessfully(string Message)
+        {
+            // Assertion that popup message record has been displayed.
+            Assert.AreEqual(profileCertsObj.GetNotification(testDriver), Message, "CT-MT has been added to your certification");
+        }
+
+        [Given(@"I click on the Certification pen icon")]
+        public void GivenIClickOnTheCertificationPenIcon()
+        {
+            profileCertsObj.EditNewCertBtn(testDriver);
+        }
+
+        [When(@"edit the data in '(.*)', '(.*)', '(.*)' and click on Add button")]
+        public void WhenEditTheDataInAndClickOnAddButton(string Certificate, string From, string Year)
+        {
+            profileCertsObj.EditCertification(testDriver, Certificate, From, Year);
+        }
+
+        [Then(@"The new row should be updated as '(.*)' successfully")]
+        public void ThenTheNewRowShouldBeUpdatedAsSuccessfully(string Message)
+        {
+            // Assertion that popup message record has been displayed.
+            Assert.AreEqual(profileCertsObj.GetNotification(testDriver), Message, "CTFL-AuT has been updated to your certification");
+        }
+
+        [When(@"I click on delete icon under Certification tab")]
+        public void WhenIClickOnDeleteIconUnderCertificationTab()
+        {
+            profileCertsObj.DeleteNewCertBtn(testDriver);
+        }
+
+        [Then(@"the Certification should have the deleted as '(.*)' successfully")]
+        public void ThenTheCertificationShouldHaveTheDeletedAsSuccessfully(string Message)
+        {
+            // Assertion that popup message record has been displayed.
+            Assert.AreEqual(profileCertsObj.GetNotification(testDriver), Message, "CTFL-AuT has been deleted from your certification");
+        }
+    }
+}
